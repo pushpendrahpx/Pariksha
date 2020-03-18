fe7d9793afcaaa4900939aadd6b2c81d440293a6
pushpendrahpx <pushpendra.hpx2001@gmail.com>
2020-03-18 22:24:51 +0530

    Permission Changes

---
+++ pariksha/src/Components/Navbar/Navbar.js
@@ -0,0 +1,14 @@
+import React,{Component} from 'react';
+class Navbar extends Component{
+    constructor(props){
+        super(props)
+
+        this.state = {
+
+        }
+    }
+
+    render(){
+        return <div>Trying Baby</div>
+    }
+}
